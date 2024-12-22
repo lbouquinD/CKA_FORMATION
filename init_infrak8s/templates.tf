d35763e686f2f96ba55133557fec39f1a5524e50
lbouquinD <laurent.bouquin2@gmail.com>
2024-12-22 19:59:47 +0100

    set  bastion hosts

--- init_infrak8s/templates.tf
+++ init_infrak8s/templates.tf
@@ -60,3 +60,29 @@ resource "null_resource" "set_hosts" {
     ]
   }
 }
+
+
+resource "null_resource" "connect_to_masters" {
+  count = aws_instance.k8sMaster.*.id
+  depends_on = [null_resource.update_hosts,local_file.hosts]
+  connection {
+    type = "ssh"
+    # Use the public_ip or private_ip attribute based on your network configuration
+    host = aws_instance.k8sMaster[count.index].private_ip
+    user = "ubuntu"  # Replace with the actual username if different
+    private_key = tls_private_key.k8s.private_key_pem
+  }
+  provisioner "file" {
+    source  = "hosts"  # local public key
+    destination  = "/tmp/hosts"  # will copy to remote VM as /tmp/test.pub
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+    "# Supprimer les balises # BEGIN TERRAFORM, # END TERRAFORM et tout ce qui est entre elles",
+    "sudo sed -i '/# BEGIN TERRAFORM/,/# END TERRAFORM/{//!d}' /etc/hosts #supression de ce qu'il y a entre ces balises ",
+    "sudo sed -i '/# BEGIN TERRAFORM/,/# END TERRAFORM/d'  /etc/hosts #supression des balises ",
+    "sudo tee -a /etc/hosts < /tmp/hosts",
+    ]
+  }
+}
