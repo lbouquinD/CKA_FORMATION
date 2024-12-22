9ffa8abb2819e2b1bbbb23e5f6f3227111afa90c
lbouquinD <laurent.bouquin2@gmail.com>
2024-12-22 20:16:52 +0100

    set  bastion hosts

--- init_infrak8s/templates.tf
+++ init_infrak8s/templates.tf
@@ -86,6 +86,7 @@ resource "null_resource" "connect_to_masters" {
     "sudo sed -i '/# BEGIN TERRAFORM/,/# END TERRAFORM/{//!d}' /etc/hosts #supression de ce qu'il y a entre ces balises ",
     "sudo sed -i '/# BEGIN TERRAFORM/,/# END TERRAFORM/d'  /etc/hosts #supression des balises ",
     "sudo tee -a /etc/hosts < /tmp/hosts",
+    "sudo hostnamectl set-hostname k8sMaster${count.index}"
     ]
   }
 }
