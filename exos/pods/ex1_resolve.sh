8a82d6c1e3755d5827531b478001d95f25ccc91a
lbouquinD <laurent.bouquin2@gmail.com>
2025-01-29 09:36:09 +0100

    [exos]  build it

--- exos/pods/ex1_resolve.sh
+++ exos/pods/ex1_resolve.sh
@@ -1,1 +1,16 @@
 #!/bin/bash  
+
+
+function get_JSON_Pods() {
+    name=$1
+    namespace=$2
+    result=$(kubectl get  po $name -n  $namespace -o  json)
+    echo  $? 
+
+}
+
+get_JSON_Pods expod2 default  
+
+
+
+
