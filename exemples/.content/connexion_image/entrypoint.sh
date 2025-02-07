b647ad0bff651e5d40bb18c04103b61605e18c4c
lbouquinD <laurent.bouquin2@gmail.com>
2025-02-07 10:59:21 +0000

    [example]  update

--- exemples/.content/connexion_image/entrypoint.sh
+++ exemples/.content/connexion_image/entrypoint.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
 if  [ -z "$NOMAPP" ]; then
-  echo -e "\033[31m La variable d'environnement   NOMAPP n'est pas défini."
+  echo -e "\033[31m La variable d'environnement   NOMAPP n'est pas défini.\033[0m"
   exit 1
 fi
 if [ -z "$NOMAPPDEST" ] ; then
-  echo -e "\033[31m La variable d'environnement NOM_APP  n'est pas défini."
+  echo -e "\033[31m La variable d'environnement NOM_APP  n'est pas défini.\033[0m"
   exit 1
 fi
 if [ -z "$NAMESPACEDEST" ] ; then
-   echo -e "\033[31m La variable d'environnement NAMESPACEDEST  n'est pas défini."
+   echo -e "\033[31m La variable d'environnement NAMESPACEDEST  n'est pas défini.\033[0m"
    exit 1
 fi
 
 if [ -z "$APPDESTPORT" ] ; then
-   echo -e "\033[38;5;214m WARNING \033[0m : La variable d'environnement APPPORT  n'est pas défini. Port par défault => 80"
+   echo -e "\033[38;5;214m WARNING \033[0m : La variable d'environnement APPPORT  n'est pas défini. Port par défault => 80 \033[0m"
    APPDESTPORT=80
 fi
 
 
 #!/bin/sh
 while true; do
-  echo -e "[ $(date) ] Je suis l'app $NOMAPP : tentative de connexion sur $NOMAPPDEST.$NAMESPACEDEST.svc.cluster.local"
+  echo -e "[ $(date) ] Je suis l'app $NOMAPP : tentative de connexion sur $NOMAPPDEST.$NAMESPACEDEST.svc.cluster.local:$APPDESTPORT"
   
   # Effectuer la requête avec un timeout de 5 secondes
-  HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 5 $NOMAPPDEST.$NAMESPACEDEST.svc.cluster.local)
+  HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 5 $NOMAPPDEST.$NAMESPACEDEST.svc.cluster.local:$APPDESTPORT)
   
   # Vérifier le code de retour
   if [ "$HTTP_CODE" -eq 200 ]; then
