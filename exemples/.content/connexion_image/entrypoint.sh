78ee138e6f9c7bf16ea5444c318d2af96b4f1063
lbouquinD <laurent.bouquin2@gmail.com>
2025-02-07 11:27:32 +0000

    [example]  update

--- exemples/.content/connexion_image/entrypoint.sh
+++ exemples/.content/connexion_image/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 if  [ -z "$NOMAPP" ]; then
-  echo -e "\033[31m La variable d'environnement   NOMAPP n'est pas défini.\033[0m"
+  echo -e "\033[31m La variable d'environnement   URL n'est pas défini.\033[0m"
   exit 1
 fi
-if [ -z "$NOMAPPDEST" ] ; then
-  echo -e "\033[31m La variable d'environnement NOM_APP  n'est pas défini.\033[0m"
+if  [ -z "$URL" ]; then
+  echo -e "\033[31m La variable d'environnement   URL n'est pas défini.\033[0m"
   exit 1
 fi
-if [ -z "$NAMESPACEDEST" ] ; then
-   echo -e "\033[31m La variable d'environnement NAMESPACEDEST  n'est pas défini.\033[0m"
-   exit 1
-fi
-
 if [ -z "$APPDESTPORT" ] ; then
    echo -e "\033[38;5;214m WARNING \033[0m : La variable d'environnement APPPORT  n'est pas défini. Port par défault => 80 \033[0m"
    APPDESTPORT=80
@@ -25,13 +20,14 @@ LISTENPORT=${LISTENPORT:-80}
 echo -e "[ $(date) ] Démarrage de Nginx sur le port $LISTENPORT"
 sed -i "s/listen       80;/listen       $LISTENPORT;/g" /etc/nginx/conf.d/default.conf
 
+
 # Démarrer Nginx en arrière-plan
 nginx -g 'daemon off;' &
 while true; do
-  echo -e "[ $(date) ] Je suis l'app $NOMAPP : tentative de connexion sur $NOMAPPDEST.$NAMESPACEDEST.svc.cluster.local:$APPDESTPORT"
+  echo -e "[ $(date) ] Je suis l'app $NOMAPP : tentative de connexion sur $URL:$APPDESTPORT"
   
   # Effectuer la requête avec un timeout de 5 secondes
-  HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 5 $NOMAPPDEST.$NAMESPACEDEST.svc.cluster.local:$APPDESTPORT)
+  HTTP_CODE=$(curl -s -o /dev/null -w "%{http_code}" --connect-timeout 5 $URL:$APPDESTPORT)
   
   # Vérifier le code de retour
   if [ "$HTTP_CODE" -eq 200 ]; then
