e6f4730b7322c68dba72f5d2900dc1b43eefa7fd
lbouquinD <laurent.bouquin2@gmail.com>
2025-02-21 09:06:31 +0000

    [exos]  secret -> Correct  ex3

--- exos/secret/ex3_test.sh
+++ exos/secret/ex3_test.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 
-RED="\e[31m"
-GREEN="\e[32m"
+RED="\e[31m\e[1m"
+GREEN="\e[32m\e[1m"
 ENDCOLOR="\e[0m"
-
-typesecret=$(kubectl get secret  -o yaml monpremiersecrettls -o jsonpath='{.type}')
+BLUE="\e[34m\e[1m"
+echo -e "${BLUE}\n\t [TEST monpremiersecrettls] \n${ENDCOLOR}"
+typesecret=$(kubectl get secret  -o yaml monpremiersecrettls -o jsonpath='{.type}' 2>/dev/null)
 if [[ "$typesecret" == "kubernetes.io/tls"  ]]; then 
-  echo -e "\t Le type de secret  est  correct ${GREEN}OK${ENDCOLOR}"
+  echo -e " Le type de secret  est  correct ${GREEN}OK${ENDCOLOR}"
 else
-  echo -e "\t Le type de secret est  correct  ${RED}KO${ENDCOLOR}" 
-  echo -e "\t \t ${RED} type de secret  trouvé: $typesecret -> secret souhaité tls ${ENDCOLOR}" 
+  echo -e " Le type de secret est  correct  ${RED}KO${ENDCOLOR}" 
+  echo -e "\t ${RED} type de secret  trouvé: $typesecret -> secret souhaité tls ${ENDCOLOR}" 
 fi
 
 
-base64cle=$(cat /tmp/ex3secret/private.key |base64)
-base64crt=$(cat /tmp/ex3secret/public.crt |base64)
+base64cle=$(cat /tmp/ex3secret/private.key |base64 |tr -d "\n")
+base64crt=$(cat /tmp/ex3secret/public.crt |base64 |tr -d "\n")
 
-tlskey=$(kubectl get secret  -o yaml monpremiersecrettls -o jsonpath='{.data.tls\.key}')
-tlscrt=$(kubectl get secret  -o yaml monpremiersecrettls -o jsonpath='{.data.tls\.crt}')
+tlskey=$(kubectl get secret  -o yaml monpremiersecrettls -o jsonpath='{.data.tls\.key}' 2>/dev/null)
+tlscrt=$(kubectl get secret  -o yaml monpremiersecrettls -o jsonpath='{.data.tls\.crt}' 2>/dev/null)
+
 
 if [[ "$base64cle" == "$tlskey"  && "$base64crt" ==  "$tlscrt" ]]; then 
-  echo -e "\t la clé et/ou le certificat sont correct ${GREEN}OK${ENDCOLOR}"
+  echo -e " la clé et/ou le certificat sont correct ${GREEN}OK${ENDCOLOR}"
 else
-  echo -e "\t La cle ou/et le certificat sont incorrect  ${RED}KO${ENDCOLOR}" 
+  echo -e " La cle ou/et le certificat sont incorrect  ${RED}KO${ENDCOLOR}" 
 fi
