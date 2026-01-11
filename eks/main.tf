8d6cdd9d8e62bb1aad9e28a61efddce2e6a8cf80
lbouquinD-gsoi <root@DESKTOP-6FQ9NNN.>
2026-01-11 13:03:45 +0100

    fix: eks deploy

--- eks/main.tf
+++ eks/main.tf
@@ -1,6 +1,13 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.40"
+    }
+  }
+}
 provider "aws" {
   region = var.region
 }
